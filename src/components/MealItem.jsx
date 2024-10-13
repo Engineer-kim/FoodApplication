72e5acd8b79b88734c6a853968e9d6d289ac5c2f
Engineer-kim <ddang0103@naver.com>
2024-10-13 17:36:23 +0900

    Feature: 상품 가격에 대한 포맷팅 적용 & 버튼 공통 컴포넌트 작성

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,3 +1,5 @@
+import { currencyFormatter } from "../util/formatting"
+import Button from "./UI/Button"
 
 
 export default function MealItem({meal}) {
@@ -8,11 +10,11 @@ export default function MealItem({meal}) {
                     <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
                     <div>
                         <h3>{meal.name}</h3>
-                        <p className="meal-item-price">{meal.price}</p>
+                        <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
                         <p className="meal-item-description">{meal.description}</p>
                     </div>
                     <p className="meal-item-action">
-                        <button>Add To Cart</button>
+                        <Button>Add To Cart</Button>
                     </p>
                 </article>
             </li>
